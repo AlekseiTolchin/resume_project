dbcc7949a9fac33af9aea51081681d1a07a3c97d
Aleksey525 <alex_tolchin@mail.ru>
2025-08-30 03:18:32 +0300

    Add method

--- src/repositories/users.py
+++ src/repositories/users.py
@@ -14,6 +14,10 @@ class UserRepository:
         result = await self.db_session.execute(select(User).where(User.id == user_id))
         return result.scalar_one_or_none()
 
+    async def get_by_email(self, email: str) -> Optional[User]:
+        result = await self.db_session.execute(select(User).where(User.email == email))
+        return result.scalar_one_or_none()
+
     async def create(self, email: str, hashed_password: str) -> User:
         db_user = User(email=email, hashed_password=hashed_password)
         self.db_session.add(db_user)
